f50875172ec5fcd14d93062c6c7efc7a7d0e8965
seoyeun0106 <sarah.mun16@gmail.com>
2023-01-06 18:12:26 +0900

    chore: 컴포넌트 분리

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,8 @@
+<template>
+   <form class="form" @submit.prevent="onSubmit">
+    <input type="text" class="input" :value="text" @input="text= $event.target.value">
+    <button class="btn">꾹</button>
+  </form>
+</template>
+<script></script>
+<style></style>
