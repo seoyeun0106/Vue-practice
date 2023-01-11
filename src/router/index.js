ec05abdfa94d6197337be01d0f055d1636f78efa
seoyeun0106 <sarah.mun16@gmail.com>
2023-01-11 17:35:59 +0900

    [feat] 라우터 설정

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import TodoList from "../components/TodoList.vue";
+import About from "@/views/About"
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+
+const routes =[
+    {   
+        path:"/todo/:id",
+        name:"Todo-List",
+        component: TodoList,
+    },{
+        path:"/about",
+        name:"About",
+        component: About
+        
+    }
+];
+
+const router = createRouter({
+ history:createWebHashHistory(),
+    routes
+})
+
+export default router;
